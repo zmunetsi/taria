743fa95fa0295c2d69f65d13a648801e72a6d677
Munetsi Zunguzira <dev@jetweb.co.za>
2020-08-11 16:02:56 +0200

    create random room ids

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,15 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var { v4: uuidv4 } = require('uuid');
 var User = require('../models/user');
+
 
 router.get('/rooms', (req, res, next) => {
 
   var username = req.query.username;
-  var roomid = req.query.roomid;
   var roomcapacity = req.query.roomcapacity;
   var roomname = req.query.roomname;
   var host = req.query.host;
+  var roomid;
 
+  console.log(host);
+
+  if(host){
+   
+    roomid = uuidv4();
+
+  }else{
+
+    roomid = req.query.roomid;   
+
+  }
 
     res.render('room', {username: username, roomid : roomid, roomcapacity: roomcapacity, roomname: roomname, host: host});
 
